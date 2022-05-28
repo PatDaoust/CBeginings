71211dcacdd94f41576166458ddbdbf4d37a9d02
Cataluna <catalunalilith2680@gmail.com>
2022-05-28 09:34:51 -0400

    implement helloworld

--- helloworld/main.c
+++ helloworld/main.c
@@ -3,6 +3,32 @@
 
 int main()
 {
-    printf("hello world\n");
+    printf("Hello World!\n");
+    printf("I am learning C.\n");
+
+//This is a single-line comment
+/*
+ This is a multi-line comment
+*/
+
+int myNum = 15;
+//printf(myNum);  // ?program stops?
+printf("%d", myNum);  // Outputs 15
+printf("\n");
+
+float myFloatNum = 5.99;   // Floating point number
+printf("%f\n", myFloatNum);
+char myLetter = 'D';       // Character
+printf("%c\n", myLetter);
+
+printf("My favorite number is: %d", myNum);
+printf("\n");
+
+int x = 5;
+int y = 6;
+int sum = x + y;
+printf("the sum is: %d", sum);
+printf("\n");
+
     return 0;
 }
