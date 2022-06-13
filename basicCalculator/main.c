3e57de5bf026cda26da20d30408d2cd429a43b6d
Cataluna <catalunalilith2680@gmail.com>
2022-06-12 20:21:43 -0400

    implement basicCalculator

--- basicCalculator/main.c
+++ basicCalculator/main.c
@@ -3,37 +3,33 @@
 
 int main()
 {
-    int num1;
-    int num2;
+    double num1;
+    double num2;
     int operation;
 
     printf("please input a number: ");
-    scanf("%d", &num1);
+    scanf("%lf", &num1);
     printf("please input another number: ");
-    scanf("%d", &num2);
+    scanf("%lf", &num2);
 
-    printf("please input 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, or 5 for modulo: ");
-    scanf("%d", operation);
+    printf("please input 1 for addition, 2 for subtraction, 3 for multiplication, or 4 for division: ");
+    scanf("%d", &operation);
 
     if (operation==1){
-        printf("%d", num1+num2);
+        printf("the sum is: %f", num1+num2);
     }
     else if (operation==2){
-        printf("%d", num1-num2);
+        printf("%f", num1-num2);
     }
     else if (operation==3){
-        printf("%d", num1*num2);
+        printf("%f", num1*num2);
     }
     else if (operation==4){
         printf("%f", num1/num2);
     }
-    else if (operation==5){
-        printf("%f", num1%num2);
-    }
     else{
         printf("that was not a valid entry. goodbye");
     }
-
 
     return 0;
 }
